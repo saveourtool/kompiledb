abdcb5b2449d90db6f219419056796fcd5d8948c
Andrey Shcheglov <andrewbass+saveourtool@gmail.com>
2023-07-10 19:57:57 +0300

    Make version catalogs accessible from precompiled script plugins.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -12,4 +12,10 @@ dependencies {
     implementation(libs.kotlin.reflect)
     implementation(libs.kotlin.gradle.plugin)
     implementation(libs.gradle.kotlin.dsl.plugins)
+
+    /*
+     * Workaround for https://github.com/gradle/gradle/issues/15383:
+     * Make version catalogs accessible from precompiled script plugins.
+     */
+    implementation(files(project.libs.javaClass.superclass.protectionDomain.codeSource.location))
 }
