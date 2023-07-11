bc6752de609f83b8eb972b1c938f9d82571b892d
Andrey Shcheglov <andrewbass+saveourtool@gmail.com>
2023-07-11 12:15:15 +0300

    Configure Maven publishing

--- buildSrc/src/main/kotlin/com/saveourtool/kompiledb/publishing-configuration.gradle.kts
+++ buildSrc/src/main/kotlin/com/saveourtool/kompiledb/publishing-configuration.gradle.kts
@@ -1,8 +1,186 @@
 package com.saveourtool.kompiledb
 
+import org.gradle.internal.logging.text.StyledTextOutput
+import org.gradle.internal.logging.text.StyledTextOutput.Style.Failure
+import org.gradle.internal.logging.text.StyledTextOutput.Style.Success
+import org.gradle.internal.logging.text.StyledTextOutputFactory
+import org.gradle.kotlin.dsl.support.serviceOf
+
 plugins {
     `maven-publish`
     signing
 }
 
 group = "com.saveourtool.kompiledb"
+version = "1.0.0-SNAPSHOT"
+
+configurePublishing()
+
+/**
+ * Configures all aspects of the publishing process.
+ */
+fun Project.configurePublishing() {
+    createPublications()
+    configureGitHubPublishing()
+    configurePublications()
+    configureSigning()
+}
+
+/**
+ * Creates the publications.
+ */
+fun Project.createPublications() {
+    publishing {
+        publications {
+            create<MavenPublication>("maven") {
+                from(components["java"])
+                suppressPomMetadataWarningsFor("testFixturesApiElements")
+                suppressPomMetadataWarningsFor("testFixturesRuntimeElements")
+            }
+        }
+    }
+}
+
+/**
+ * Configures GitHub Packages as the publish destination.
+ */
+fun Project.configureGitHubPublishing() =
+    publishing {
+        repositories {
+            maven {
+                name = "GitHub"
+                url = uri("https://maven.pkg.github.com/saveourtool/kompiledb")
+                credentials {
+                    username = findProperty("gpr.user") as String? ?: System.getenv("GITHUB_ACTOR")
+                    password = findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
+                }
+            }
+        }
+    }
+
+/**
+ * Configures all publications. The publications must already exist.
+ */
+fun Project.configurePublications() {
+    configure<PublishingExtension> {
+        publications.withType<MavenPublication>().configureEach {
+            pom {
+                val project = this@configurePublications
+
+                name.set(project.name)
+                description.set(project.description ?: project.name)
+                url.set("https://github.com/saveourtool/kompiledb")
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://opensource.org/license/MIT")
+                        distribution.set("repo")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("0x6675636b796f75676974687562")
+                        name.set("Andrey Shcheglov")
+                        email.set("shcheglov.av@phystech.edu")
+                    }
+                }
+                scm {
+                    url.set("https://github.com/saveourtool/kompiledb")
+                    connection.set("scm:git:https://github.com/saveourtool/kompiledb.git")
+                    developerConnection.set("scm:git:git@github.com:saveourtool/kompiledb.git")
+                }
+            }
+        }
+    }
+}
+
+/**
+ * Enables signing of the artifacts if the `signingKey` project property is set.
+ *
+ * Should be explicitly called after each custom `publishing {}` section.
+ */
+fun Project.configureSigning() {
+    System.getenv("GPG_SEC")?.let {
+        extra.set("signingKey", it)
+    }
+    System.getenv("GPG_PASSWORD")?.let {
+        extra.set("signingPassword", it)
+    }
+
+    if (hasProperty("signingKey")) {
+        /*
+         * GitHub Actions.
+         */
+        configureSigningCommon {
+            useInMemoryPgpKeys(property("signingKey") as String?, findProperty("signingPassword") as String?)
+        }
+    } else if (
+        hasProperties(
+            "signing.keyId",
+            "signing.password",
+            "signing.secretKeyRingFile",
+        )
+    ) {
+        /*-
+         * Pure-Java signing mechanism via `org.bouncycastle.bcpg`.
+         *
+         * Requires an 8-digit (short form) PGP key id and a present `~/.gnupg/secring.gpg`
+         * (for gpg 2.1, run
+         * `gpg --keyring secring.gpg --export-secret-keys >~/.gnupg/secring.gpg`
+         * to generate one).
+         */
+        configureSigningCommon()
+    } else if (hasProperty("signing.gnupg.keyName")) {
+        /*-
+         * Use an external `gpg` executable.
+         *
+         * On Windows, you may need to additionally specify the path to `gpg` via
+         * `signing.gnupg.executable`.
+         */
+        configureSigningCommon {
+            useGpgCmd()
+        }
+    }
+}
+
+/**
+ * @param useKeys the block which configures the PGP keys. Use either
+ *   [SigningExtension.useInMemoryPgpKeys], [SigningExtension.useGpgCmd], or an
+ *   empty lambda.
+ * @see SigningExtension.useInMemoryPgpKeys
+ * @see SigningExtension.useGpgCmd
+ */
+@Suppress(
+    "MaxLineLength",
+    "SpreadOperator",
+)
+fun Project.configureSigningCommon(useKeys: SigningExtension.() -> Unit = {}) {
+    configure<SigningExtension> {
+        useKeys()
+        val publications = extensions.getByType<PublishingExtension>().publications
+        val publicationCount = publications.size
+        val message = "The following $publicationCount publication(s) are getting signed: ${publications.map(Named::getName)}"
+        val style = when (publicationCount) {
+            0 -> Failure
+            else -> Success
+        }
+        styledOut(logCategory = "signing").style(style).println(message)
+        sign(*publications.toTypedArray())
+    }
+}
+
+/**
+ * Creates a styled text output.
+ */
+fun Project.styledOut(logCategory: String): StyledTextOutput =
+    serviceOf<StyledTextOutputFactory>().create(logCategory)
+
+/**
+ * Determines if this project has all the given properties.
+ *
+ * @param propertyNames the names of the properties to locate.
+ * @return `true` if this project has all the given properties, `false` otherwise.
+ * @see Project.hasProperty
+ */
+fun Project.hasProperties(vararg propertyNames: String): Boolean =
+    propertyNames.asSequence().all(this::hasProperty)
