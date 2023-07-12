5d960793c0748ee3ab2c1f765db34047ae3c1a88
Andrey Shcheglov <andrewbass+saveourtool@gmail.com>
2023-07-12 11:51:24 +0300

    Enable version inference using the Reckon plug-in (#45)
    Version 0.13.2 is the last one compatible with Java 8.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
+    id("com.saveourtool.kompiledb.versioning-configuration")
     id("com.saveourtool.kompiledb.publishing-configuration")
 }
 
 group = "com.saveourtool.kompiledb"
-version = "1.0.1-SNAPSHOT"
